7231da6f35147f573441f3ac42352668e2061612
Sachin P <66244820+IamGroot111@users.noreply.github.com>
2022-05-27 21:31:26 +0530

    Update mp.py

--- mp.py
+++ mp.py
@@ -2,22 +2,20 @@ Arr = [ (3,"you"), (2,"are"), (4,"?"), (1,"how")]
 class messque:
     def __init__(self):
         self.que=dict()
+        self.k=1
        
     def addtoque(self,x):
         self.que[x[0]]=x[1]
 
     def printque(self):
-        c=1
-        while c:
-            if(self.que.get(c,"rip")!="rip"):
-                print(self.que.get(c,"rip"))
-                c+=1
-            else:
-                break
+        while(self.que.get(self.k,"rip")!="rip"):
+            print(self.que.get(self.k,"rip"))
+            self.k+=1
+        
 
 mess1=messque()    
 
 for i in Arr:
+    c=1
     mess1.addtoque(i)
-
-mess1.printque()
+    mess1.printque()

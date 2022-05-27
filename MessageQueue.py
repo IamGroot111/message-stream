0723e3f9533540c599fb11f4bfd5d3da2cb1abd9
Sachin P <66244820+IamGroot111@users.noreply.github.com>
2022-05-27 23:22:49 +0530

    Update MessageQueue.py

--- MessageQueue.py
+++ MessageQueue.py
@@ -1,17 +1,17 @@
 Arr = [ (3,"you"), (2,"are"), (4,"?"), (1,"how")]
 class MessageQueue:
     def __init__(self):
-        self.__que=dict()
+        self.__queue=dict()
         self.__current=1
 
     def __check_and_print(self):
-        while(self.__current in self.__que):
-            print(self.__que[self.__current])
+        while(self.__current in self.__queue):
+            print(self.__queue[self.__current])
             self.__current+=1
 
-    def add_to_que(self,data):
+    def add_to_queue(self,data):
         i,mess=data
-        self.__que[i]=mess
+        self.__queue[i]=mess
         self.__check_and_print()
     
         
@@ -20,5 +20,5 @@ mess1=MessageQueue()
 print(mess1.__dict__ )
 
 for i in Arr:
-    mess1.add_to_que(i)
+    mess1.add_to_queue(i)
 print(mess1.__dict__ )
